a0a83f11309a1bfbb6027b002395e49dae2c6943
davit.chanturia <chanturiadato@gmail.com>
2024-11-16 18:26:24 +0400

    style: empty state for user account quizzes tab

--- components/User/UserQuizzes.vue
+++ components/User/UserQuizzes.vue
@@ -1,95 +1,102 @@
 <template>
-  <Card>
-    <template #content>
-      <DataTable
-        v-model:editing-rows="editingRows"
-        :value="quizzes"
-        striped-rows
-        edit-mode="row"
-        data-key="id"
-        :pt="{
-          table: { style: 'min-width: 50rem' },
-          column: {
-            bodycell: ({ state }: any) => ({
-              style:
-                state['d_editing'] &&
-                'padding-top: 0.75rem; padding-bottom: 0.75rem',
-            }),
-          },
-        }"
-        @row-edit-save="onRowEditSave"
-      >
-        <Column field="title" header="Title">
-          <template #editor="{ data, field }">
-            <InputText v-model="data[field]" fluid />
-          </template>
-        </Column>
+  <div>
+    <Card v-if="quizzes.length > 0" class="w-full">
+      <template #content>
+        <DataTable
+          v-model:editing-rows="editingRows"
+          :value="quizzes"
+          striped-rows
+          edit-mode="row"
+          data-key="id"
+          :pt="{
+            table: { style: 'min-width: 50rem' },
+            column: {
+              bodycell: ({ state }: any) => ({
+                style:
+                  state['d_editing'] &&
+                  'padding-top: 0.75rem; padding-bottom: 0.75rem',
+              }),
+            },
+          }"
+          @row-edit-save="onRowEditSave"
+        >
+          <Column field="title" header="Title">
+            <template #editor="{ data, field }">
+              <InputText v-model="data[field]" fluid />
+            </template>
+          </Column>
 
-        <Column field="category" header="Category">
-          <template #editor="{ data, field }">
-            <Select
-              v-model="data[field].name"
-              :options="categoryOptions"
-              option-value="name"
-              option-label="name"
-              placeholder="Select a Status"
-              fluid
-            >
-              <template #option="slotProps">
-                <Tag :value="slotProps.option.name" />
-              </template>
-            </Select>
-          </template>
-          <template #body="slotProps">
-            <Tag :value="slotProps.data.category.name" />
-          </template>
-        </Column>
+          <Column field="category" header="Category">
+            <template #editor="{ data, field }">
+              <Select
+                v-model="data[field].name"
+                :options="categoryOptions"
+                option-value="name"
+                option-label="name"
+                placeholder="Select a Status"
+                fluid
+              >
+                <template #option="slotProps">
+                  <Tag :value="slotProps.option.name" />
+                </template>
+              </Select>
+            </template>
+            <template #body="slotProps">
+              <Tag :value="slotProps.data.category.name" />
+            </template>
+          </Column>
 
-        <Column field="difficulty" header="Difficulty">
-          <template #editor="{ data, field }">
-            <Select
-              v-model="data[field]"
-              :options="difficultyOptions"
-              option-label="name"
-              option-value="value"
-              placeholder="Select a difficulty level"
-              fluid
-            >
-              <template #option="slotProps">
-                <Tag :value="slotProps.option.name" />
-              </template>
-            </Select>
-          </template>
+          <Column field="difficulty" header="Difficulty">
+            <template #editor="{ data, field }">
+              <Select
+                v-model="data[field]"
+                :options="difficultyOptions"
+                option-label="name"
+                option-value="value"
+                placeholder="Select a difficulty level"
+                fluid
+              >
+                <template #option="slotProps">
+                  <Tag :value="slotProps.option.name" />
+                </template>
+              </Select>
+            </template>
 
-          <template #body="slotProps">
-            <Tag :value="slotProps.data.difficulty" />
-          </template>
-        </Column>
+            <template #body="slotProps">
+              <Tag :value="slotProps.data.difficulty" />
+            </template>
+          </Column>
 
-        <Column field="is_active" header="Status">
-          <template #editor="{ data, field }">
-            <Checkbox v-model="data[field]" binary variant="filled" />
-          </template>
-          <template #body="slotProps">
-            <i
-              v-if="slotProps.data.is_active"
-              class="pi pi-check"
-              style="color: green"
-            ></i>
-            <i v-else class="pi pi-times" style="color: red"></i>
-          </template>
-        </Column>
+          <Column field="is_active" header="Status">
+            <template #editor="{ data, field }">
+              <Checkbox v-model="data[field]" binary variant="filled" />
+            </template>
+            <template #body="slotProps">
+              <i
+                v-if="slotProps.data.is_active"
+                class="pi pi-check"
+                style="color: green"
+              ></i>
+              <i v-else class="pi pi-times" style="color: red"></i>
+            </template>
+          </Column>
 
-        <Column field="created_at" header="Created at"></Column>
+          <Column field="created_at" header="Created at"></Column>
 
-        <Column
-          :row-editor="true"
-          style="width: 10%; min-width: 8rem"
-          body-style="text-align:center"
-        ></Column>
-      </DataTable>
-    </template>
-  </Card>
+          <Column
+            :row-editor="true"
+            style="width: 10%; min-width: 8rem"
+            body-style="text-align:center"
+          ></Column>
+        </DataTable>
+      </template>
+    </Card>
+
+    <div v-else>
+      <div>You have not created quizzes yet</div>
+      <Button class="mt-5" label="Create quiz" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
